6918c44a4030107b2ae0424827acbfd91f22f1b8
Ekow Nii Saki Sackey <ekow.sackey@amalitech.com>
2025-12-09 15:52:33 +0000

    add javadocs to repository classes

--- src/main/repository/ProjectList.java
+++ src/main/repository/ProjectList.java
@@ -10,6 +10,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * This class uses a List as a data storage for projects.
+ * <p>This class currently represents a repository of "database level" methods for data storage, retrieval and deletion
+ * controlled by the Project Service</p>
+ * <p>addProject adds a project to the list.</p>
+ * <p>getProjects returns a list of all projects in the list.</p>
+ * <p>getByID finds a project by a given ID and returns the Project.</p>
+ * <p>getByID finds a project by a given ID and returns the Project.</p>
+ * <p>getByType finds projects of a given type and returns a list of Projects</p>
+ * <p>getAllTasks returns all the tasks in all the projects in the list.</p>
+ * <p>getTaskByID returns a task in the list by searching the projects for a matching ID.</p>
+ */
 public class ProjectList {
     private final List<Project> projects = new ArrayList<Project>();
 
--- src/main/repository/UserList.java
+++ src/main/repository/UserList.java
@@ -6,8 +6,18 @@ import main.models.User;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class uses a List as a data storage for users.
+ * <p>This class currently represents a repository of "database level" methods for data storage, retrieval and deletion,
+ * controlled by the User Service.</p>
+ * <p>addUser adds a user to the list.</p>
+ * <p>getUsers returns a list of users.</p>
+ * <p>findByUsername finds a user by a given username.</p>
+ */
+
 public class UserList {
     private final List<User> users = new ArrayList<User>();
+
 
     public void addUser(User user){
         for (User u: users){
