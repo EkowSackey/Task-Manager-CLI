613407d5fd514325ca57e8b8c6d46ca270fbd039
Ekow Nii Saki Sackey <ekow.sackey@amalitech.com>
2025-12-16 11:32:09 +0000

    cleanup commented code

--- src/main/repository/ProjectList.java
+++ src/main/repository/ProjectList.java
@@ -10,10 +10,10 @@ import java.util.*;
 
 
 /**
- * This class uses a List as a data storage for projects.
+ * This class uses a hashMap as a data storage for projects.
  * <p>This class currently represents a repository of "database level" methods for data storage, retrieval and deletion
  * controlled by the Project Service</p>
- * <p>addProject adds a project to the list.</p>
+ * <p>addProject adds a project to the map.</p>
  * <p>getProjects returns a list of all projects in the list.</p>
  * <p>getByID finds a project by a given ID and returns the Project.</p>
  * <p>getByID finds a project by a given ID and returns the Project.</p>
@@ -23,20 +23,20 @@ import java.util.*;
  */
 public class ProjectList {
 
-    private final HashMap<String, Project> prjs = new HashMap<>();
+    private final HashMap<String, Project> projects = new HashMap<>();
 
     public synchronized void addProject(Project project){
-//        projects.add(project);
-        prjs.put(project.getID(), project);
+
+        projects.put(project.getID(), project);
     }
 
     public synchronized List<Project> getProjects(){
-        return List.copyOf(prjs.values());
+        return List.copyOf(projects.values());
 
     }
 
     public synchronized Project getByID(String ID){
-        Project p = prjs.get(ID);
+        Project p = projects.get(ID);
 
         if (p != null){
             return p;
@@ -45,7 +45,7 @@ public class ProjectList {
     }
 
     public synchronized List<Project> getByType(String type){
-        return prjs.values().stream()
+        return projects.values().stream()
                 .filter(p->p.getType().equals(type))
                 .toList();
     }
@@ -54,14 +54,14 @@ public class ProjectList {
 
         if (min > max) throw new InvalidRangeException("Invalid range");
 
-        return prjs.values().stream()
+        return projects.values().stream()
                 .filter(x-> x.getBudget() >= min && x.getBudget() <=max)
                 .toList();
 
     }
 
     public synchronized List<Task> getAllTasks(){
-        return prjs.values().stream()
+        return projects.values().stream()
                 .flatMap(p-> p.getTasks().stream())
                 .toList();
     }
